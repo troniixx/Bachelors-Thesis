7285ed0967aa60b0ad6b70f5193b3c85a52920b3
troniixx <meroert55@gmail.com>
2025-11-24 13:04:27 +0100

    added gitignore

--- src/scripts/failsafe_pipeline.sh
+++ src/scripts/failsafe_pipeline.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Copyright (c) 2025 Mert Erol, University of Zurich
 # Licensed under the Academic and Educational Use License (AEUL) – see LICENSE file for details.
+
+# NOTE: This script is only there for failsafe. The main runner is run_pipeline.sh
 set -euo pipefail
 
 ############################################
